6f197325557b2cbd667d636b30c25f8627cf9eca
Jaime André <jaimeaugusto11@gmail.com>
2025-03-12 18:30:29 +0100

    Descrição do que foi alterado

--- src/app/(backoffice)/dashboard/page.tsx
+++ src/app/(backoffice)/dashboard/page.tsx
@@ -7,7 +7,7 @@ import React from 'react'
 export default function page() {
   return (
     <div>
-      <Heading title='Dashboard Overview'/>
+      <Heading title='Dashboard Overview hosana'/>
      {/* large Cards */}
     {  <LargeCards/>}
      {/* small Cards */}
