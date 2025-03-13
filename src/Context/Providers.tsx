5802c2b1b6856a67c2d590b0bbe3895905270aa0
Jaime André <jaimeaugusto11@gmail.com>
2025-03-13 02:10:19 +0100

    Actualização clientes

--- src/Context/Providers.tsx
+++ src/Context/Providers.tsx
@@ -1,10 +1,12 @@
 'use client'
-import React from 'react'
+import React, { ReactNode } from 'react'
 import {ThemeProvider} from "next-themes"
 import {Toaster} from "react-hot-toast"
+interface ProvidersProps {
+  children: ReactNode;
+}
 
-
-export default function Providers({children}) {
+export default function Providers({children}: ProvidersProps) {
   return (
     <ThemeProvider attribute="class" defaultTheme='dark'>
       
